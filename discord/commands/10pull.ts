aa8656465017adc7de2970d4e06914f76ee536b5
zojize <jeffzo01@qq.com>
2025-07-14 18:16:41 -0500

    refactor: renamed some commands files and changed descriptions

---
+++ discord/commands/10pull.ts
@@ -0,0 +1,41 @@
+/**
+ * @name 寻访十次
+ * @description 模拟十连寻访
+ */
+export default defineSlashCommand(async (pool: string) => {
+  describeOption(pool, {
+    name: '寻访卡池',
+    description: '指定十连寻访的卡池名称。',
+    autocomplete: searchGachaPool,
+  })
+
+  const interaction = useInteraction()!
+  const { gachaServerTable, gachaClientTable } = await getGachaTables()
+
+  const gachaClientPool = gachaClientTable[pool]
+  const gachaServerPool = gachaServerTable[pool]
+
+  if (!gachaClientPool || !gachaServerPool) {
+    return '未找到指定的寻访卡池，请检查卡池名称是否正确。'
+  }
+
+  const userId = interaction.user.id
+  const executor = getGachaExecutor(userId, pool, gachaServerPool, gachaClientPool)
+
+  const results: NonNullable<ReturnType<typeof executor['doGachaOnce']>>[] = []
+
+  for (let i = 0; i < 10; i++) {
+    const result = executor.doGachaOnce()
+    if (!result && i === 0) {
+      return '寻访卡池剩余抽取次数已耗尽'
+    }
+    else if (!result) {
+      break
+    }
+    results.push(result)
+  }
+
+  return `本次寻访卡池为：${gachaClientPool.gachaPoolName}
+已寻访次数：${Object.values(executor.state.results).reduce((acc, cur) => acc + cur, 0)}
+${formatGachaPull(results)}`
+})
---
+++ discord/commands/1pull.ts
@@ -0,0 +1,34 @@
+/**
+ * @name 寻访一次
+ * @description 模拟单次寻访
+ */
+export default defineSlashCommand(async (pool: string) => {
+  describeOption(pool, {
+    name: '寻访卡池',
+    description: '指定单次寻访的卡池名称。',
+    autocomplete: searchGachaPool,
+  })
+
+  const interaction = useInteraction()!
+  const { gachaServerTable, gachaClientTable } = await getGachaTables()
+
+  const gachaClientPool = gachaClientTable[pool]
+  const gachaServerPool = gachaServerTable[pool]
+
+  if (!gachaClientPool || !gachaServerPool) {
+    return '未找到指定的寻访卡池，请检查卡池名称是否正确。'
+  }
+
+  const userId = interaction.user.id
+  const executor = getGachaExecutor(userId, pool, gachaServerPool, gachaClientPool)
+
+  const result = executor.doGachaOnce()
+
+  if (!result) {
+    return '寻访卡池剩余抽取次数已耗尽'
+  }
+
+  return `本次寻访卡池为：${gachaClientPool.gachaPoolName}
+已寻访次数：${Object.values(executor.state.results).reduce((acc, cur) => acc + cur, 0)}
+你抽到了：${formatGachaPull(result)}`
+})
